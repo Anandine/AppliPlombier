3d86820c668c4e37c2ba965f0706733cea0857a0
anais <ana-angels74@hotmail.fr>
2017-10-02 18:48:42 +0200

    third commit

--- app/src/main/java/fr/appli/anais/appliplombier/SousCatActivity.java
+++ app/src/main/java/fr/appli/anais/appliplombier/SousCatActivity.java
@@ -7,6 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+
 public class SousCatActivity extends AppCompatActivity {
 
     @Override
@@ -24,5 +33,40 @@ public class SousCatActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        /*String in="";
+        JSONObject reader = new JSONObject(in);
+        try {
+            JSONArray sys  = reader.getJSONArray("sous cat1");
+            String text1_cat1 = sys.get(1).toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+
+
+        /*Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1){
+               writer.write(buffer, 0, n);
+            }
+        } finally {
+            is.close();
+        }
+        String json = writer.toString();*/
+        String json = null;
+        try{
+            InputStream is = getResources().openRawResource(R.raw.test);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+        //return json;
     }
 }
