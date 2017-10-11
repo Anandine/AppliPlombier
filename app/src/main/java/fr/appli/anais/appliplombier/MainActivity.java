6e074809a08141ee7876f8e4c36af17982fdff63
anais <ana-angels74@hotmail.fr>
2017-10-11 15:20:00 +0200

    On est connecté et on essaie de télécharger le json dans Mes Documents

--- app/src/main/java/fr/appli/anais/appliplombier/MainActivity.java
+++ app/src/main/java/fr/appli/anais/appliplombier/MainActivity.java
@@ -4,13 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -79,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(I);
             }
         });
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute("https://hyppo.neocities.org/test.json");
     }
     public boolean isConnected(){
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -89,4 +103,65 @@ public class MainActivity extends AppCompatActivity {
         else
             return false;
     }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+       }
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+    }
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+    public static void updateJSON(InputStream is){
+
+        File targetFile = new File("src/main/resources/raw/test.tmp");
+        try {
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            OutputStream outStream = new FileOutputStream(targetFile);
+            outStream.write(buffer);
+        }catch (IOException ie){
+            Log.d("STATE", "Problème dans la mise à jour du fichier JSON");
+        }
+    }
+
 }
